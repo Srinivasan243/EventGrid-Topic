dcd95c6146282af25524e75959f19035279c0a08
Srinivasan243 <53461619+Srinivasan243@users.noreply.github.com>
2021-06-15 18:31:36 +0530

    Add files via upload

---
+++ Message.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SendEventGrid.Utility
+{
+    public class Message<T>
+    {
+        public string Id { get;  set; }
+
+        public string EventType { get; set; }
+
+        public string Subject { get; set; }
+
+        public string EventTime { get;  set; }
+
+        public List<T> Data { get; set; }
+
+      
+    }
+
+    public class PasTracking
+    {
+        public string primaryId { get; set; }
+
+        public string secondaryId { get; set; }
+
+        public string action { get; set; }
+
+        public Context context { get; set; }
+
+        
+    }
+
+    public class Context
+    {
+        public string id { get; set; }
+
+        public string type { get; set; }
+
+        public string orderId { get; set; }
+
+        public string store { get; set; }
+
+        public string query { get; set; }
+
+        public string source { get; set; }
+
+        public string targetSystem { get; set; }
+
+        public int quantity { get; set; }
+
+        public double revenue { get; set; }
+
+        public bool searchAsYouType { get; set; }
+
+
+    }
+}
